45c3e6333e3cf3710b454efef012cfe763ba8ee0
Алена <acanthacheae555@yandex.ru>
2026-06-19 22:13:41 +0300

    124

---
+++ src/test/java/SimpleAutoTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleAutoTest {
+    @Test
+    void test01() {
+        open("http://92.51.36.108:7777/sl.qa");
+        $("body").shouldHave(text("Учебные приложения"));
+    }
+    @Test
+    void test02() {
+        open("https://ru.wikipedia.org/wiki/Selenium");
+        $("body").shouldHave(text("Промышленные приложения"));
+    }
+}
